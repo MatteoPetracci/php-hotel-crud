25f13ae2f4cff6297a2a44cc3cbb826aed4c9c6a
Matteo <matteop18@gmail.com>
2020-03-04 15:22:25 +0100

    cartella show includendo i vari file

---
+++ show/database.php
@@ -0,0 +1,9 @@
+<!-- Utilizzo questo file solo per creare la connessione -->
+<?php
+  include __DIR__ .'/../env.php';
+  // Connessione al mio database
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  // Controllo della connessione
+  if ($conn && $conn->connect_error) {
+    echo "Connection failed: " . $conn->connect_error; die();
+  }
---
+++ show/server.php
@@ -0,0 +1,22 @@
+<?php
+  include 'database.php';
+  // Salvo in una variabile il GET
+  $roomId = $_GET['id'];
+  // Seleziono da stanze dove l'id è uguale a quello salvato nella variabile
+  $sql = "SELECT * FROM `stanze` WHERE `id` = $roomId";
+  $result = $conn->query($sql);
+
+  if ($result && $result->num_rows > 0) {
+    $rooms = [];
+    while($row = $result->fetch_assoc()) {
+      $rooms[] = $row;
+      var_dump($rooms);
+
+    }
+  } elseif ($result) {
+    echo "0 results";
+  } else {
+    echo "query error";
+  }
+  $conn->close();
+?>
